669d05c4135fa7a417df6cbc7cbc42efabdd5963
betterlogics <108340548+betterlogics@users.noreply.github.com>
2023-05-05 17:50:20 +0500

    add BUSD option

--- src/components/Exchange_modal/Exchange_modal.jsx
+++ src/components/Exchange_modal/Exchange_modal.jsx
@@ -4,6 +4,7 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import selling_pic from "../Assets/ethr.png";
+import selling_pic_2 from "../Assets/binance-usd-busd-seeklogo.com.svg";
 import buying_pic from "../Assets/buying_ethr.png";
 
 function Exchange_modal(props) {
@@ -24,21 +25,32 @@ function Exchange_modal(props) {
             Exchange
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body className="ps-5">
+        <Modal.Body className="">
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label className="all_gary exten_headings">
                 Selling
               </Form.Label>
-              <div className="d-flex align-items-center gap-2">
-                <Form.Control
-                  type="text"
-                  name="selling"
-                  className="slng fr_inside"
+              <div className="d-flex align-items-center justify-content-evenly">
+                <Form.Control type="text" name="selling"
+                  className="slng fr_inside ps-3"
                 />
                 <h6 className="mx mb-0">MAX</h6>
-                <img src={selling_pic} alt="" className="selling_cor" />
-                <h6 className="mb-0 bnb_selling">BNB</h6>
+                <div>
+                  <Form.Select aria-label="Default select example" className="block_coin text-white">
+                    {/* <option>Open this select men</option> */}
+                    <option value="1" className="text-white d-flex gap-2">
+                      <div>
+                        <img src={selling_pic} alt="" className="selling_cor w-100"/>
+                        <p className="mb-0 text-white">BNB</p>
+                      </div>
+                    </option>
+                    <option value="2" className="text-white d-flex gap-2">
+                      <img src={selling_pic_2} alt="" className="w-100" />
+                      <h6 className="mb-0 bnb_selling text-white">BUSD</h6>
+                    </option>
+                  </Form.Select>
+                </div>
               </div>
             </Form.Group>
 
@@ -46,13 +58,13 @@ function Exchange_modal(props) {
               <Form.Label className="all_gary exten_headings">
                 Buying
               </Form.Label>
-              <div className="d-flex align-items-center gap-2">
-                <Form.Control type="text" name="buying" className="slng" />
+              <div className="d-flex align-items-center justify-content-evenly">
+                <Form.Control type="text" name="buying" className="slng ps-3" />
                 <img src={buying_pic} alt="" className="buying_cor" />
                 <h6 className="mb-0 bnb_selling">UAWIN</h6>
               </div>
             </Form.Group>
-            <div className="ps-5"> 
+            <div className="ps-5">
               <Button className="swp text-black ms-5" type="submit">
                 Swap
               </Button>
